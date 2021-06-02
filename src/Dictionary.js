6f29ed91f78b9739dcfbeede41b77f82a7b8bec7
marielacordeiro <marielapontes@icloud.com>
2021-06-01 22:07:39 -0300

    beginning to add the photos api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,20 +2,31 @@ import axios from "axios";
 import React, { useState } from "react";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
     let [keyword, setKeyword] = useState(props.defaultKeyword);
     let [results, setResults] = useState(null);
     let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState(null);
 
-    function handleResponse (response) {
+    function handleDictionaryResponse (response) {
         setResults(response.data[0]);
+    }
+    
+    function handlePexelsResponse(response) {
+        setPhotos(response.data.photos);
     }
 
     function search() {
         // documentation: https://dictionaryapi.dev/
-        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-        axios.get(apiUrl).then(handleResponse);
+        let DictionaryApiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+        axios.get(DictionaryApiUrl).then(handleDictionaryResponse);
+
+        let pexelsApiKey = "563492ad6f9170000100000139446773701e4ceab5638f9eb36e4a1d";
+        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+        let headers = {Authorization : `Bearer ${pexelsApiKey}`};
+        axios.get(pexelsApiUrl, {headers: headers}).then(handlePexelsResponse);
     }
 
     function handleSubmit(event) {
@@ -47,7 +58,7 @@ export default function Dictionary(props) {
             </div>
             </section>
             <Results results= {results}/>
-
+            <Photos photos = {photos} />
         </div>
       );
     } else {
---
+++ src/Photos.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Photos(props) {
+    if (props.photos) {
+        return "Hello";
+    } else {
+        return null;
+    }
+}
